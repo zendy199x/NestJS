d229f42868e21711eae46ae13d8f2abe8eac0746
Zendy <nguyenduc.051194@gmail.com>
2023-01-09 00:24:42 +0700

    add docker compose file

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,20 +1,20 @@
 import { Module } from '@nestjs/common';
-import { TasksModule } from './tasks/tasks.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { TasksModule } from './tasks/tasks.module';
 
 @Module({
   imports: [
-    TasksModule,
     TypeOrmModule.forRoot({
       type: 'postgres',
       host: 'localhost',
       port: 5432,
       username: 'postgres',
       password: 'postgres',
-      database: 'task-management',
+      database: 'task-management-nestjs',
       autoLoadEntities: true,
       synchronize: true,
     }),
+    TasksModule,
   ],
 })
 export class AppModule {}
