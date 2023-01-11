a434ed0395bf492df11cc7a9889516c3b511ce2e
Zendy <nguyenduc.051194@gmail.com>
2023-01-12 01:48:17 +0700

    Signup, validate and error handling username conflicts

---
+++ src/auth/dto/auth-credentials.dto.ts
@@ -0,0 +1,28 @@
+import {
+  IsNotEmpty,
+  IsString,
+  Matches,
+  MaxLength,
+  MinLength,
+} from 'class-validator';
+
+export class AuthCredentialsDto {
+  @IsString()
+  @MinLength(8)
+  @MaxLength(20)
+  @IsNotEmpty()
+  username: string;
+
+  @IsString()
+  @MinLength(8)
+  @MaxLength(20)
+  @IsNotEmpty()
+  //   Passwords will contain at least 1 upper case letter
+  // Passwords will contain at least 1 lower case letter
+  // Passwords will contain at least 1 number or special character
+  // There is no length validation (min, max) in this regex!
+  @Matches(/((?=.*\d)|(?=.*\W+))(?![.\n])(?=.*[A-Z])(?=.*[a-z]).*$/, {
+    message: 'Password is to weak',
+  })
+  password: string;
+}
--- src/auth/users.repository.ts
+++ src/auth/users.repository.ts
@@ -1,10 +1,32 @@
+import { AuthCredentialsDto } from './dto/auth-credentials.dto';
 import { DataSource, Repository } from 'typeorm';
-import { Injectable } from '@nestjs/common';
+import {
+  ConflictException,
+  Injectable,
+  InternalServerErrorException,
+} from '@nestjs/common';
 import { User } from './user.entity';
 
 @Injectable()
 export class UsersRepository extends Repository<User> {
   constructor(dataSource: DataSource) {
     super(User, dataSource.createEntityManager());
+  }
+
+  async createUser(authCredentialsDto: AuthCredentialsDto): Promise<void> {
+    const { username, password } = authCredentialsDto;
+
+    const user = this.create({ username, password });
+
+    try {
+      await this.save(user);
+    } catch (error) {
+      // Duplicate username
+      if (error.code === '23505') {
+        throw new ConflictException('Username already exists');
+      } else {
+        throw new InternalServerErrorException();
+      }
+    }
   }
 }
