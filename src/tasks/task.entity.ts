56f38d618384a7eddd295ef2e65e11bd088ba857
Zendy <nguyenduc.051194@gmail.com>
2023-01-14 21:51:20 +0700

    Tasks and Users - Database relation

--- src/tasks/task.entity.ts
+++ src/tasks/task.entity.ts
@@ -1,4 +1,11 @@
-import { Entity, BaseEntity, PrimaryGeneratedColumn, Column } from 'typeorm';
+import { User } from '../auth/user.entity';
+import {
+  Entity,
+  BaseEntity,
+  PrimaryGeneratedColumn,
+  Column,
+  ManyToOne,
+} from 'typeorm';
 import { TaskStatus } from './task-status.enum';
 
 @Entity()
@@ -14,4 +21,7 @@ export class Task extends BaseEntity {
 
   @Column()
   status: TaskStatus;
+
+  @ManyToOne((_type) => User, (user) => user.tasks, { eager: false })
+  user: User;
 }
